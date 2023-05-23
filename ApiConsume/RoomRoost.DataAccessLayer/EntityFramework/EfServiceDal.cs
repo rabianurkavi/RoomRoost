d99334f4b95291a56a1a903484cda65ca7232cf4
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-05-23 18:24:20 +0300

    Entityframework

---
+++ ApiConsume/RoomRoost.DataAccessLayer/EntityFramework/EfServiceDal.cs
@@ -0,0 +1,17 @@
+﻿using RoomRoost.DataAccessLayer.Abstract;
+using RoomRoost.DataAccessLayer.Concrete;
+using RoomRoost.DataAccessLayer.Repositories;
+using RoomRoost.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomRoost.DataAccessLayer.EntityFramework
+{
+    public class EfServiceDal:GenericRepository<Service>, IServiceDal
+    {
+        public EfServiceDal(Context context) : base(context) { }
+    }
+}
