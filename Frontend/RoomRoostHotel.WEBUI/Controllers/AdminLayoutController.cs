c657dc363390bdcacc4357636916437ce01f6b2f
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-05-25 16:01:38 +0300

    loading of frontend theme and creating adminlayout(UI LAYERS)

---
+++ Frontend/RoomRoostHotel.WEBUI/Controllers/AdminLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoomRoostHotel.WEBUI.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult _AdminLayout()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/RoomRoostHotel.WEBUI/Controllers/StaffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoomRoostHotel.WEBUI.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
