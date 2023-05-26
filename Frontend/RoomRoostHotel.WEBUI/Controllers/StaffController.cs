568a5b7e306b0a160294414f1d656cfd8f30510d
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-05-26 15:30:39 +0300

    consume api

--- Frontend/RoomRoostHotel.WEBUI/Controllers/StaffController.cs
+++ Frontend/RoomRoostHotel.WEBUI/Controllers/StaffController.cs
@@ -1,11 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RoomRoostHotel.WEBUI.Models.Staff;
 
 namespace RoomRoostHotel.WEBUI.Controllers
 {
     public class StaffController : Controller
     {
-        public IActionResult Index()
+        readonly private IHttpClientFactory _httpClientFactory;
+        public StaffController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();//bir tane istemci oluştur
+            //istemci aracılığıyla, belirtilen URI'ye bir GET isteği gönderilir
+            var responseMessage = await client.GetAsync("http://localhost:5086/api/Staff");
+            //eğer başarılı bir kod dönerse
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                //responseMessage.Content.ReadAsStringAsync() koduyla yanıtın içeriği asenkron olarak okunur ve jsondata değişkenine
+                //atanır. Bu içerik, genellikle bir JSON verisi olarak gelir.
+                var jsondata =await responseMessage.Content.ReadAsStringAsync();
+                //dönüştürme işlemi
+                //JsonConvert.DeserializeObject<List<StaffViewModel>>(jsondata) ifadesi, jsondata içeriğini List<StaffViewModel>
+                //türünde bir nesneye dönüştürür. Bu dönüşüm, JSON verisini C# nesnelerine uygun şekilde ayrıştırır.
+                var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsondata);
+                return View(values);
+            }
             return View();
         }
     }
---
+++ Frontend/RoomRoostHotel.WEBUI/Models/Staff/StaffViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace RoomRoostHotel.WEBUI.Models.Staff
+{
+    public class StaffViewModel
+    {
+        public int StaffId { get; set; }
+        public string StaffName { get; set; }
+        public string StaffTitle { get; set; }
+    }
+}
