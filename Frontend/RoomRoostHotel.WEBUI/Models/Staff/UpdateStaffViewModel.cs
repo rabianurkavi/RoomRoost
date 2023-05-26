3177d68ce991ceb55a4cf3231cf82bd8d0dc38e8
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-05-26 16:50:52 +0300

    updateStaff

---
+++ Frontend/RoomRoostHotel.WEBUI/Models/Staff/UpdateStaffViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace RoomRoostHotel.WEBUI.Models.Staff
+{
+    public class UpdateStaffViewModel
+    {
+        public int StaffId { get; set; }
+        public string StaffName { get; set; }
+        public string StaffTitle { get; set; }
+        public string Facebook { get; set; }
+        public string İnstagram { get; set; }
+        public string Twitter { get; set; }
+    }
+}
