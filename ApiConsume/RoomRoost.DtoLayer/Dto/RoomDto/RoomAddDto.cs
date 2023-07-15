6345b8788e0a50aad69d68ab63d59b2810146d51
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-07-15 08:00:06 +0300

    Dto

---
+++ ApiConsume/RoomRoost.DtoLayer/Dto/RoomDto/RoomAddDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomRoost.DtoLayer.Dto.RoomDto
+{
+    public class RoomAddDto
+    {
+        [Required(ErrorMessage ="Lütfen oda numarasını giriniz.")]
+        public string RooomNumber { get; set; }
+        public string RoomCoverImage { get; set; }
+        [Required(ErrorMessage ="Lütfen fiyat bilgisi giriniz.")]
+        public int RoomPrice { get; set; }
+        [Required(ErrorMessage = "Lütfen oda başlığı  giriniz.")]
+        public string RoomTitle { get; set; }
+        [Required(ErrorMessage = "Lütfen yatak sayısını giriniz.")]
+        public string BedCount { get; set; }
+        [Required(ErrorMessage = "Lütfen banyo sayısı giriniz.")]
+        public string BathCount { get; set; }
+        public string Wifi { get; set; }
+        public string RoomDescription { get; set; }
+    }
+}
---
+++ ApiConsume/RoomRoost.DtoLayer/Dto/RoomDto/UpdateRoomDto.cs
@@ -0,0 +1,30 @@
+﻿using DiscordRPC.Message;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomRoost.DtoLayer.Dto.RoomDto
+{
+    public class UpdateRoomDto
+    {
+        public int RoomID { get; set; }
+        [Required(ErrorMessage = "Lütfen oda numarasını giriniz.")]
+        public string RooomNumber { get; set; }
+        public string RoomCoverImage { get; set; }
+        [Required(ErrorMessage = "Lütfen fiyat bilgisi giriniz.")]
+        [MaxLength(20,ErrorMessage="Bir hata meydana geldi, lütfen tekrar deneyin.")]
+        public int RoomPrice { get; set; }
+        [Required(ErrorMessage = "Lütfen oda başlığı  giriniz.")]
+        [MaxLength(50, ErrorMessage="En fazla 50 karakter girebilirsiniz.")]
+        public string RoomTitle { get; set; }
+        [Required(ErrorMessage = "Lütfen yatak sayısını giriniz.")]
+        public string BedCount { get; set; }
+        [Required(ErrorMessage = "Lütfen banyo sayısı giriniz.")]
+        public string BathCount { get; set; }
+        public string Wifi { get; set; }
+        public string RoomDescription { get; set; }
+    }
+}
