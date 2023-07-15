4111fa82f726af93953bdf740c87282f84c8378a
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-07-15 08:08:41 +0300

    mapping

---
+++ ApiConsume/RoomRoost.WebApi/Mapping/AutoMapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using RoomRoost.DtoLayer.Dto.RoomDto;
+using RoomRoost.EntityLayer.Concrete;
+
+namespace RoomRoost.WebApi.Mapping
+{
+    public class AutoMapperConfig: Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<RoomAddDto, Room>();
+            CreateMap<Room, RoomAddDto>();
+            CreateMap<UpdateRoomDto, Room>().ReverseMap();//Makes reverse mapping, namely Room,UpdateRoomDto
+
+                
+        }
+    }
+}
