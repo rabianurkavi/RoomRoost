f6fbeaf31317cbd68fd99ad823ed28474ae4450f
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-09-11 14:53:15 +0300

    AppUserDto

---
+++ Frontend/RoomRoostHotel.WEBUI/Controllers/RegisterController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RoomRoost.EntityLayer.Concrete;
+using RoomRoostHotel.WEBUI.Dtos.RegisterDto;
+
+namespace RoomRoostHotel.WEBUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(CreateAppUserDto createAppUser)
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontend/RoomRoostHotel.WEBUI/Dtos/RegisterDto/CreateAppUserDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoomRoostHotel.WEBUI.Dtos.RegisterDto
+{
+    public class CreateAppUserDto
+    {
+        [Required(ErrorMessage = "Ad alanı gereklidir")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Soyad alanı gereklidir.")]
+        public string SurName { get; set; }
+        [Required(ErrorMessage = "Kullanıcı adı gereklidir.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Mail gereklidir.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Şifre gereklidir.")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Şifre tekrarı gereklidir.")]
+        [Compare("Password",ErrorMessage ="Şifreler Uyuşmuyor")]
+        public string ConfirmPassword { get; set; }
+    }
+}
