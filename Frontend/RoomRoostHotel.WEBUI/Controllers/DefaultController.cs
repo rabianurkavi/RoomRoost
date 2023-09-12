30dbd281411afcc69fac089924b80155ad52a119
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-09-12 15:28:16 +0300

    Hotel layout

---
+++ Frontend/RoomRoostHotel.WEBUI/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoomRoostHotel.WEBUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
