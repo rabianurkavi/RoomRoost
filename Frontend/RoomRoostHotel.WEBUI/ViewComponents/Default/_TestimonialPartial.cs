ef40344cf17c13a2c1e27db47fea849800eb49b0
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-09-16 08:57:17 +0300

    View Components use for Trailer,Service,Testimonial in Index

---
+++ Frontend/RoomRoostHotel.WEBUI/ViewComponents/Default/_TestimonialPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoomRoostHotel.WEBUI.ViewComponents.Default
+{
+    public class _TestimonialPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
