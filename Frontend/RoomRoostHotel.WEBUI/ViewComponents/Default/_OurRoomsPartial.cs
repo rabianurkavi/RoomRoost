18de05d5082c8b1422d705bbb655b8ab85ed128c
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-09-16 08:38:29 +0300

    View Component use for About Us, Our Rooms in Index

---
+++ Frontend/RoomRoostHotel.WEBUI/ViewComponents/Default/_OurRoomsPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoomRoostHotel.WEBUI.ViewComponents.Default
+{
+    public class _OurRoomsPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
