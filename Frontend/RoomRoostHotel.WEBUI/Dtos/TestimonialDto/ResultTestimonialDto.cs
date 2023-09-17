4c1b9e377c0d3319c4b30a01d2ed41440f8e7e82
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-09-17 09:08:27 +0300

    Testimonial arrangements

---
+++ Frontend/RoomRoostHotel.WEBUI/Dtos/TestimonialDto/ResultTestimonialDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RoomRoostHotel.WEBUI.Dtos.TestimonialDto
+{
+    public class ResultTestimonialDto
+    {
+        public int TestimonialID { get; set; }
+        public string TestimonialImage { get; set; }
+        public string TestimonialName { get; set; }
+        public string TesminonialTitle { get; set; }
+        public string TestimonialDescription { get; set; }
+    }
+}
--- Frontend/RoomRoostHotel.WEBUI/ViewComponents/Default/_TestimonialPartial.cs
+++ Frontend/RoomRoostHotel.WEBUI/ViewComponents/Default/_TestimonialPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RoomRoostHotel.WEBUI.Dtos.ServiceDto;
+using RoomRoostHotel.WEBUI.Dtos.TestimonialDto;
 
 namespace RoomRoostHotel.WEBUI.ViewComponents.Default
 {
     public class _TestimonialPartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        readonly private IHttpClientFactory _httpClientFactory;
+        public _TestimonialPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();//bir tane istemci oluştur
+            var responseMessage = await client.GetAsync("http://localhost:5086/api/Testimonial");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultTestimonialDto>>(jsondata);
+                return View(values);
+            }
+
             return View();
         }
     }
