e4e475f7563e9ccc266e7594b2b898261fc87966
Rabia Nur Kavi <kavirabiaa@gmail.com>
2023-09-17 08:53:55 +0300

    Service arrangements in ViewComponent

--- Frontend/RoomRoostHotel.WEBUI/ViewComponents/Default/_ServicePartial.cs
+++ Frontend/RoomRoostHotel.WEBUI/ViewComponents/Default/_ServicePartial.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RoomRoostHotel.WEBUI.Dtos.RoomDto;
+using RoomRoostHotel.WEBUI.Dtos.ServiceDto;
 
 namespace RoomRoostHotel.WEBUI.ViewComponents.Default
 {
     public class _ServicePartial:ViewComponent
     {
-        public IViewComponentResult Invoke()
+        readonly private IHttpClientFactory _httpClientFactory;
+        public _ServicePartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();//bir tane istemci oluştur
+            var responseMessage = await client.GetAsync("http://localhost:5086/api/Service");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDto>>(jsondata);
+                return View(values);
+            }
+
             return View();
         }
+      
     }
 }
