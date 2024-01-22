ccad987261d9d882a8d8012d08febfd1a770efef
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-01-22 11:33:52 +0300

    'Booking' page edit

---
+++ Frontend/RoomRoostHotel.WEBUI/Controllers/BookingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoomRoostHotel.WEBUI.Controllers
+{
+    public class BookingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
