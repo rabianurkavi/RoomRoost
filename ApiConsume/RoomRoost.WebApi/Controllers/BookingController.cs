678277667087380cfbf4f51d704678167ec29231
Rabia Nur Kavi <kavirabiaa@gmail.com>
2024-01-23 19:54:33 +0300

    'Booking' transactions

---
+++ ApiConsume/RoomRoost.WebApi/Controllers/BookingController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RoomRoost.BusinessLayer.Abstract;
+using RoomRoost.EntityLayer.Concrete;
+
+namespace RoomRoost.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookingController : ControllerBase
+    {
+        private readonly IBookingService _bookingService;
+        public BookingController(IBookingService bookingService)
+        {
+            _bookingService = bookingService;
+        }
+        [HttpGet]
+        public IActionResult BookingList()
+        {
+            var values = _bookingService.TGetAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult AddBooking(Booking booking)
+        {
+            _bookingService.TAdd(booking);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBooking(int id)
+        {
+            var values = _bookingService.TGetById(id);
+            _bookingService.TDelete(values);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult UpdateBooking(Booking booking)
+        {
+            _bookingService.TUpdate(booking);
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetBooking(int id)
+        {
+            var values = _bookingService.TGetById(id);
+            return Ok(values);
+        }
+    }
+}
---
+++ Frontend/RoomRoostHotel.WEBUI/ViewComponents/Booking/_BookingCoverPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoomRoostHotel.WEBUI.ViewComponents.Booking
+{
+    public class _BookingCoverPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
